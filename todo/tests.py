27e3b65504761f9ee91b5bf7d5ba342f2d0fbf6b
dilip <dilipmaharjann@gmail.com>
2018-08-06 14:54:34 -0400

    assert False

--- todo/tests.py
+++ todo/tests.py
@@ -4,5 +4,8 @@ from django.test import TestCase
 # Create your tests here.
 
 class TestCiExample(TestCase):
-    def test_true(self):
+    def test_equal(self):
         self.assertEqual("hello", "hello")
+
+    def test_false(self):
+        self.assertFalse(False)
